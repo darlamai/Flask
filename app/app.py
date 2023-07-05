0c2e6eb183f687e1d243c3a40ee72cc9e959306f
darlamai <darlynludena963@gmail.com>
2023-07-05 17:48:55 -0500

    Creación de API y definición de nuevas rutas

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 #detrás de la página o backend
 from flask import Flask, render_template, request
 import pickle
+from flask import jsonify 
 
 
 ##creación de aplicación
 app=Flask(__name__)
+print(__name__)
 model= pickle.load(open('model.pkl','rb'))
 
-
+#definir una ruta
 @app.route('/') #@=modifica el comportamiento de una función, da el url / (página raíz) que desata la función
 def home():
     return render_template('index.html')
@@ -23,7 +25,22 @@ def predecir():
     return render_template('index.html', 
                            prediccion_texto=f'El vehículo con {puertas} puertas, {desplazamiento} metros de desplazamiento, {tonelaje} toneladas, {capacidad} pasajeros tiene kilometraje de {output} km')
 
+@app.route('/app/v1/users', methods=['GET','POST'])
+def users_action():
+    print(request.form)
+    print(request.method)
+    if(request.method=="POST"):
+        return "guardame en la base"
+    else:
+        return "no guardes"
+    
 
+allUser=[{"id":0, "nombre":"damian"},
+         {"id":1, "nombre":"pepe"}]
+
+@app.route('/app/v2/users/<id>', methods=["GET","POST"])
+def user(id):
+    return jsonify(allUser[int(id)])
 
 if __name__=='__main__':
     app.run(debug=True) #modo de depuración
